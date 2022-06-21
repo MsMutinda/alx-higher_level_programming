61b75f4901bfd492e0dd5b3d5fb2dc29e491cd76
Juliet <MsMutinda@users.noreply.github.com>
2022-06-21 19:09:31 +0300

    classes & objects

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines a class Square."""
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """
+        Initializes a new Square instance.
+        Args: size (int): The size of the new square.
+        """
+        self.__size = size
